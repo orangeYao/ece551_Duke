3fd5ac381622fdde83887aa82d8c5d2cbf5786fe
zx52 <zx52@duke.edu>
2017-08-31 23:49:31 -0400

    2

--- 012_complex_ty/mytypes.c
+++ 012_complex_ty/mytypes.c
@@ -6,11 +6,11 @@ typedef struct _complex_num_t complex_num_t;
 
 enum _animal_t
 {
-    CAT;
-    FISH;
-    SNAKE;
-    DOG;
-    HORSE;
+    CAT,
+    FISH,
+    SNAKE,
+    DOG,
+    HORSE
 };
 typedef enum _animal_t animal_t
 
