28dd7bd15b499ab7469b47e4b6748b7b1c706647
zx52 <zx52@duke.edu>
2017-09-01 21:22:36 -0400

    1

---
+++ 023_testing2/tryCombine.py
@@ -0,0 +1,7 @@
+
+for a in range(1, 6):
+    for b in range(1, 6):
+        for c in range(1, 6):
+            for d in range(1, 6):
+                for e in range(1, 6):
+                    print str(a)+str(b)+str(c)+str(d)+str(e)
