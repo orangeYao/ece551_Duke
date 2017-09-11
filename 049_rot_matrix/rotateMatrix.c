127a126c91d7adb0ced30fbecd15aaac79403f42
zx52 <zx52@duke.edu>
2017-09-11 16:21:10 -0400

    1

--- 049_rot_matrix/rotateMatrix.c
+++ 049_rot_matrix/rotateMatrix.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define N 10
+
+
+void reportError(char * e)
+{
+  fprintf(stderr,"%s", e);
+  exit(EXIT_FAILURE);
+}
 
 void rotateMatrix(FILE *f)
 {
   char c[N+2][N+2]; // consider \n and \0
   int idx = 0;
   while(fgets(c[idx], N+2, f) !=NULL)
+  {
+    char * newline_ptr = strchr(c[idx], '\n');
+
+    if(newline_ptr - &(c[idx][0]) != 10)
+      reportError("Line length in file mismatch expectation \n");
     idx++;
+  }
+
+  if (idx != 10)
+    reportError("Lines in file mismatch expectation \n");
 
   for (int j=0; j<10; j++)
   {
@@ -24,25 +41,16 @@ void rotateMatrix(FILE *f)
 int main(int argc, char ** argv)
 {
   if (argc != 2)
-  {
-    fprintf(stderr, "Provide one input file \n");
-    return 1;
-  }
+    reportError("Provide one input file \n");
 
   FILE * f = fopen(argv[1], "r"); 
   if (f == NULL)
-  {
-    fprintf(stderr, "Failed to open file \n");
-    return 1;
-  }
+    reportError("Failed to open file \n");
 
   rotateMatrix(f);
 
   if (fclose(f) != 0)
-  {
-    fprintf(stderr, "Failed to close file \n");
-    return 1;
-  }
+    reportError("Failed to close file \n");
 
-  return 0;
+  return EXIT_SUCCESS;
 }
