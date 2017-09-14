5a016a3be0e2daad690c512221b7229e1889d35b
zx52 <zx52@duke.edu>
2017-09-13 20:36:59 -0400

    1

--- 052_fix_vg_encr/encrypt.c
+++ 052_fix_vg_encr/encrypt.c
@@ -4,24 +4,26 @@
 #include <string.h>
 
 void encrypt(FILE * f, int key, FILE * outfile){
-  char * line;
-  size_t sz;
-  while (getline(&line,&sz, f) >= 0) {
+  char * line = "damn";
+  size_t sz = 0;
+  while (getline(&line,&sz, f) > 0) {
     char * ptr = line;
+
     while (*ptr != '\0') {
       int c = *ptr;
       if (isalpha(c)) {
-	c = tolower(c);
-	c -= 'a';
-	c += key;
-	c %= 26;
-	c += 'a';
+	    c = tolower(c);
+	    c -= 'a';
+	    c += key;
+	    c %= 26;
+	    c += 'a';
       }
       *ptr = c;
       ptr++;
     }
     fprintf(outfile, "%s", line);
   }
+  free(line);
 }
 
 int main(int argc, char ** argv) {
@@ -40,11 +42,18 @@ int main(int argc, char ** argv) {
     return EXIT_FAILURE;
   }
   //outfileNAme is argv[2] + ".txt", so add 4 to its length.
-  char * outFileName = malloc((strlen(argv[2]) + 4) * sizeof(*outFileName));
+  char * outFileName = malloc((strlen(argv[2]) + 5) * sizeof(*outFileName));
   strcpy(outFileName, argv[2]);
   strcat(outFileName, ".enc");
   FILE * outFile = fopen(outFileName, "w");
+
+  if (outFile == NULL) {
+    perror("Could not open file");
+    return EXIT_FAILURE;
+  }
+
   encrypt(f,key, outFile);
+
   if (fclose(outFile) != 0) {
     perror("Failed to close the input file!");
     return EXIT_FAILURE;
@@ -54,5 +63,6 @@ int main(int argc, char ** argv) {
     return EXIT_FAILURE;
   }
 
+  free(outFileName);
   return EXIT_SUCCESS;
 }
