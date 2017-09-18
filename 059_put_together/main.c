dfedaa8ac6a3c0f692cd2664a0b833d309c118ad
zx52 <zx52@duke.edu>
2017-09-18 19:04:32 -0400

    2

--- 059_put_together/main.c
+++ 059_put_together/main.c
@@ -37,6 +37,13 @@ counts_t * countFile(const char * filename, kvarray_t * kvPairs) {
 int main(int argc, char ** argv) {
   //WRITE ME (plus add appropriate error checking!)
  //read the key/value pairs from the file named by argv[1] (call the result kv)
+ 
+  if (argc < 2)
+  {
+    perror("at least two file as input!");
+    return EXIT_FAILURE;
+  }
+
   kvarray_t * pairs = readKVs(argv[1]);
  //count from 2 to argc (call the number you count i)
  for (int i=2; i<argc; i++)
