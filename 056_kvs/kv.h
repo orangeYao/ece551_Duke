db19295854bb2ce056dd382f0a2c162be54062af
zx52 <zx52@duke.edu>
2017-09-18 13:08:20 -0400

    1

--- 056_kvs/kv.h
+++ 056_kvs/kv.h
@@ -4,6 +4,8 @@
 
 struct _kvpair_t { 
   //DEFINE ME!
+  char* key;
+  char* value;
 
 };
 typedef struct _kvpair_t kvpair_t;
@@ -11,6 +13,8 @@ typedef struct _kvpair_t kvpair_t;
 struct _kvarray_t { 
   //DEFINE ME!
 
+  kvpair_t** array;
+  int length;
 
 };
 typedef struct _kvarray_t kvarray_t;
