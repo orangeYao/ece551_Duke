2416365c4110297b5e57f1ae442f4ad80ca7e14a
zx52 <zx52@duke.edu>
2017-09-18 13:49:06 -0400

    1

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -5,4 +5,17 @@
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+
+  char * ptr = strstr(inputName, ".txt");
+  if (ptr == NULL)
+      return NULL;
+
+  char * for_return;
+
+  size_t len = strlen(inputName);  
+  for_return = malloc(len + 10);
+
+  strncpy(for_return, inputName, len);
+  strcpy(for_return + len, ".count");
+  return for_return;
 }
