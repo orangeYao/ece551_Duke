9e277e05e2a8ee356b6f75e0f67e2e5e87da375e
zx52 <zx52@duke.edu>
2017-10-11 13:09:41 -0400

    1

--- 067_point/main.cpp
+++ 067_point/main.cpp
@@ -2,6 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <math.h>
+
+Point::Point() : x(0), y(0)
+{
+}
+
+
+void Point::move(double dx, double dy)
+{
+  x += dx;
+  y += dy;
+}
+
+double Point::distanceFrom(const Point & p)
+{
+  return pow(pow((x - p.x), 2) + pow((y - p.y), 2), 0.5);
+}
+
 
 char * skipWs (char * line) {
   while(*line != '\0' && isspace(*line)) {
