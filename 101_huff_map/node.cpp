8e348bef03e183bc8af17e9969b1e29752e45e60
zx52 <zx52@duke.edu>
2017-11-05 19:41:54 -0500

    1

--- 101_huff_map/node.cpp
+++ 101_huff_map/node.cpp
@@ -1,5 +1,18 @@
 #include "node.h"
+using namespace std;
+
 void Node::buildMap(BitString b, std::map<unsigned,BitString> & theMap) {
-  //WRITE ME!
+  //b.plusOne()
+  if (sym == NO_SYM) //not leaf, don't add  
+  {
+    left -> buildMap(b.plusZero(), theMap);
+    right-> buildMap(b.plusOne(), theMap);
+  }  
+  else 
+  {
+    theMap.insert(pair<unsigned,BitString> (sym, b));
+  }
 }
+
+
 
